dcfdcaf0633ae177f5dd01a13521bc9f5b62ba07
evgeniyPP <amenat12@mail.ru>
2020-01-03 23:25:13 +0300

    default mongo server connection set

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+;(async () => {
+  try {
+    await mongoose.connect(config.get('mongoURI'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+  } catch (e) {
+    console.log('Server Error', e.message)
+    process.exit(1)
+  }
+})()
+
+const PORT = config.get('port') || 5000
+
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
