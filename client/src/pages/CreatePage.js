8d91a03a35ba3665d3aae59c79b885cf9cf38aab
evgeniyPP <amenat12@mail.ru>
2020-01-04 23:56:51 +0300

    create page created

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import useHttp from '../hooks/http.hook'
+import AuthContext from '../context/AuthContext'
 
-export default () => (
-  <div>
-    <h2>Create Page</h2>
-  </div>
-)
+export default () => {
+  const auth = useContext(AuthContext)
+  const history = useHistory()
+  const { request } = useHttp()
+  const [link, setLink] = useState('')
+
+  useEffect(() => {
+    if (window.M) {
+      window.M.updateTextFields() // makes inputs active
+    }
+  }, [])
+
+  const handleGenerate = async () => {
+    try {
+      const data = await request(
+        '/api/links/generate',
+        'POST',
+        { from: link },
+        {
+          Authorization: `Bearer ${auth.token}`
+        }
+      )
+      history.push(`/detail/${data.link._id}`)
+    } catch (e) {}
+  }
+
+  const handleChange = event => setLink(event.target.value)
+  const handlePress = event => event.key === 'Enter' && handleGenerate()
+
+  return (
+    <div id="create-page" className="row">
+      <div className="col s8 offset-s2">
+        <div className="input-field">
+          <input
+            type="text"
+            id="link"
+            placeholder="Вставьте ссылку"
+            value={link}
+            onChange={handleChange}
+            onKeyPress={handlePress}
+          />
+          <label htmlFor="link">Вставьте ссылку</label>
+        </div>
+        <button className="btn yellow darken-4" onClick={handleGenerate}>
+          Сгенерировать
+        </button>
+      </div>
+    </div>
+  )
+}
